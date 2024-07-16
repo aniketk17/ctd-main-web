85751dcc7d3702f23db0515f2011c703dbf8a980
Sujal Bhor <bhorsujal@gmail.com>
2024-07-16 17:08:35 -0400

    Add getProfile and updateProfile APIs and routes Created a new file controllers/profile.controller.js

---
+++ controllers/profile.controller.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const User = require('../models/user.model.js');
+
+const getProfile = async (req, res) => {
+    try {
+        const userID = req.user.id;
+        const user = await User.findByPk(userID);
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        res.status(200).json({ user });
+    }
+    catch (error) {
+        console.error('Error fetching user profile:', error);
+        res.status(500).json({ message: 'Server error' });
+    }
+}
+
+const updateProfile = async (req, res) => {
+    try {
+        const userID = req.user.id;
+        const { first_name, last_name, phone_number } = req.body;
+        const user = await User.findByPk(userID);
+        
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        // Update user profile
+        user.first_name = first_name || user.first_name;
+        user.last_name = last_name || user.last_name;
+        user.phone_number = phone_number || user.phone_number;
+
+        await user.save();
+        
+        res.status(200).json({ message: 'Profile updated successfully', user });
+    }
+    catch (error) {
+        console.error('Error updating user profile:', error);
+        res.status(500).json({ message: 'Server error' });
+    }
+}
+
+module.exports = { getProfile, updateProfile };
--- routes/api.routes.js
+++ routes/api.routes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const { authenticateToken } = require('../middlewares/auth.middleware.js');
-const cartController = require('../controllers/cart.controller.js')
+const cartController = require('../controllers/cart.controller.js');
+const profileController = require('../controllers/profile.controller.js');
 
 router.get('/dashboard', authenticateToken, (req, res) => {
   res.json({ message: 'Welcome to the dashboard!' });
@@ -12,5 +13,7 @@ router.post('/add_cart', authenticateToken, cartController.addCart);
 router.get('/view_cart', authenticateToken, cartController.viewCart);
 router.post('/delete/:eventName', authenticateToken, cartController.deleteCartItem);
 router.post('/delete_cart', authenticateToken, cartController.deleteCart);
+router.get('/profile', authenticateToken, profileController.getProfile);
+router.patch('/profile', authenticateToken, profileController.updateProfile);
 
 module.exports = router;
