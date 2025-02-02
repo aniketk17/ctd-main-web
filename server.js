7283a3eda1eeb5257f59f0d80fe74c72d08651d3
Avadhutgiri <azureuser@mainweb.akcxftcjrtse5ihu2fbosrk1sc.rx.internal.cloudapp.net>
2025-02-02 11:57:08 +0000

    Update Server.js

--- server.js
+++ server.js
@@ -14,9 +14,9 @@ const app = express();
 app.set('trust proxy', 1);  // Trust the proxy for getting real IPs
 
 const allowedOrigins = [
-  'https://ctd.credenz.co.in',  // Exact match
+  'https://credenz.co.in',  // Exact match
   /^https:\/\/.*\.credenz\.co\.in$/,
-  'http://localhost:5173'  // Any subdomain of credenz.co.in
+    // Any subdomain of credenz.co.in
 ];
 
 const corsOptions = {
